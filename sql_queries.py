62b7fc6e58c74411b6de3b46fdb400b532554866
Jennifer Arty <jenniferarty@Jennifers-MacBook-Pro.local>
2018-07-09 17:10:22 -0400

    Done.

--- sql_queries.py
+++ sql_queries.py
@@ -1,25 +1,13 @@
 # Write your SQL queries inside the strings below.  If you choose to write your queries on multiple lines, make sure to wrap your query inside """triple quotes""".  Use "single quotes" if your query fits on one line.
 
 def select_hero_names_and_squad_names_of_heroes_belonging_to_a_team():
-    return """Write
-              your
-              query
-              here"""
+    return " SELECT superheroes.name, squads.name from superheroes JOIN squads on superheroes.squad_id = squads.id;"
 
 def reformatted_query():
-    return """Write
-              your
-              query
-              here"""
+    return "SELECT superheroes.name, squads.name AS team from superheroes JOIN squads on superheroes.squad_id = squads.id ORDER BY team;"
 
 def all_superheroes():
-    return """Write
-              your
-              query
-              here"""
+    return "SELECT superheroes.name, superheroes.superpower, squads.name AS team from superheroes  LEFT JOIN squads on superheroes.squad_id = squads.id ORDER BY team;"
 
 def all_squads():
-    return """Write
-              your
-              query
-              here"""
+    return "SELECT squads.name, Count(superheroes.name) AS num_of_members FROM squads LEFT JOIN superheroes on superheroes.squad_id = squads.id GROUP BY squads.name;"
